8242d085ab3a17eb94db16c6962a73d508063aea
Mateo Micciche <mateo.micciche@gmail.com>
2021-04-11 04:18:43 +0000

    Solving an issue where directory is accidently submitting

--- react-starter/src/ListItem.js
+++
@@ -1,9 +0,0 @@
-import { useState, useRef, useEffect } from 'react';
-
-// Function that lists items, used for listing the users in the game
-export function ListItem(props){
-
-    return <li>
-        {props.name}
-    </li>
-}
