034a5c3b03ae08ab48a4fee7b4039562e1daee3f
Mateo Micciche <mateo.micciche@gmail.com>
2021-04-11 05:33:44 +0000

    Login and Logout button are now functional. May make a separate logout component to make code cleaner.

--- react-starter-code/src/Options.js
+++ react-starter-code/src/Options.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { GoogleLogin } from 'react-google-login';
+import React, { useState, useRef, useEffect } from 'react';
+import { GoogleLogout } from 'react-google-login';
+import Login from './Login';
 
+// These two lines load environmental variables from .env
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Fills clientID variable with API ID key
+const clientID = process.env.REACT_APP_GOOGLE_ID;
 
 function Options() {
+    
+  const [status, setStatus] = useState(true);
+  
+  // If the user logs in, status is set to false
+  // Sends user back to main page
+  const onSuccess = (res) => {
+    setStatus(!status);
+  };
   
   return (
-    <div>
-        SUCCESSFUL TRANSFER
+  <div>
+  {status ? (
+    <body>
+        SUCCESSFUL TRANSFER<br/><br/>
+        <GoogleLogout
+            clientId={process.env.REACT_APP_GOOGLE_ID}
+            buttonText="Log out"
+            onLogoutSuccess={onSuccess}
+        ></GoogleLogout>
+    </body>
+    ) : <Login />}
     </div>
   );
 }
