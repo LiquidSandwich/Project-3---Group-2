f091d00e4aa05b7c04da2d47f79a024b396927fc
Joncarlos Perdomo <jp692@jnit.edu>
2021-04-19 08:37:38 +0000

    Added Replay button

--- react-starter-code/src/Results.js
+++ react-starter-code/src/Results.js
@@ -1,6 +1,11 @@
+import GameMode from './GameMode.js';
+import React, { useState } from 'react';
 
 export function Results(props) {
     const { answerStats } = props;
+    const { userData } = props;
+    const [restart, setRestart] = useState(true);
+    
     console.log("RESULTS "+ answerStats)
     const handleStats = (answer) => {
         if(answer == true) {
@@ -9,13 +14,24 @@ export function Results(props) {
             <div> FALSE </div>
         }
     }
+    
+    const replay = () => {
+        setRestart(!restart);
+    }
+    
     return (
-        <div className="stats">
-            <div className="answer_results">
-				{answerStats.map((answerChoice, index) => (
-				<div> {index+1}. {answerChoice} </div>
-				))}
-			</div>
+        <div>
+        {restart ? (
+            <div className="stats">
+                <div className="answer_results">
+    				{answerStats.map((answerChoice, index) => (
+    				<div> {index+1}. {answerChoice} </div>
+    				))}
+    			</div>
+    			<button className="button" onClick={()=>replay()}>Replay</button>
+            </div>
+            ) :
+            <GameMode userData={userData}/>}
         </div>
     );
 }
