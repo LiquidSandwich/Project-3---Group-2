06b8e744bc3e0111db3a30becebf1070a01aa3a5
Mateo Micciche <mateo.micciche@gmail.com>
2021-04-28 04:12:33 +0000

    Page now allows user to change their color and fonts for game. The fonts only effect questions and other pages elements besides buttons currently. These changes only last for the users web session. Once they close the app, these changes disappear. Changed some css styling for buttons on Custom.js page

--- src/Custom.js
+++ src/Custom.js
@@ -12,12 +12,57 @@ function Custom(props) {
     props.custom();
   };
   
+  const colorChanger = (event) => {
+    if (event.target.value == "Light Blue") {
+      document.body.style.backgroundColor = "#31a9e2"
+    }
+    else {
+      document.body.style.backgroundColor = event.target.value;
+    }
+  };
+  
+  const fontChanger = (event) => {
+    if (event.target.value == "Default") {
+      document.body.style.fontFamily = 'Catamaran';
+    }
+    else {
+      document.body.style.fontFamily = event.target.value;
+    }
+  };
+  
+  
+  
   return (
     <div>
       <button type="button" className="exit" onClick={onSuccess}> 
-      <img src="https://www.freeiconspng.com/uploads/white-curved-arrow-transparent-11.png" width="70" height="50" alt="submit" /> 
+        <img src="https://www.freeiconspng.com/uploads/white-curved-arrow-transparent-11.png" width="70" height="50" alt="submit" /> 
       </button>
-      YOOOOOOO
+      <div>
+      Select Color: {' '}
+       <select onChange={colorChanger} name="trivia_category" >
+            <option selected="selected">Choose</option>
+            <option value="Light Blue">Light Blue</option>
+            <option value="Red">Red</option>
+            <option value="Green">Green</option>
+            <option value="Orange">Orange</option>
+            <option value="Purple">Purlple</option>
+            <option value="Indigo">Indigo</option>
+            <option value="Pink">Pink</option>
+        </select>
+        
+        <br/>
+        <br/>
+        Select Font: {' '}
+        <select onChange={fontChanger} name="trivia_category" >
+            <option selected="selected">Choose</option>
+            <option value="Default">Default</option>
+            <option value="serif">Serif</option>
+            <option value="sans-serif">Sans-Serif</option>
+            <option value="monospace">Monospace</option>
+            <option value="cursive">Cursive</option>
+            <option value="system-ui">System-ui</option>
+        </select>
+        </div>
     </div>
     )
 }
