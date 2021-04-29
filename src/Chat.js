ee8cebedf2b38021b4273dd4f566836dd8415561
Venkata Sunkara <jnanatejaswini@gmail.com>
2021-04-29 09:23:23 +0000

    implemented basic chat feature with chat icon

--- src/Chat.js
+++ src/Chat.js
@@ -1,33 +1,63 @@
-// import React, { useState, useEffect, useRef } from 'react';
-// // import PropTypes from 'prop-types';
-// import { socket } from './Socket';
+import React, { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { socket } from './Socket';
 
-// // These two lines load environmental variables from .env
-// const dotenv = require('dotenv');
+// These two lines load environmental variables from .env
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-function Chat() {
-//   const [chatMessages, setMessages] = useState([]);
-//   const chatInput = useRef(null);
-//   const [players, setPlayers] = useState([]);
+function Chat(props) {
+  const [chatMessages, setMessages] = useState([]);
+  const chatInput = useRef(null);
+  //   const [players, setPlayers] = useState([]);
 
-//   const onEnterMessage = () => {
-//     if (inputRef)
-//   }
+  const onChat = () => {
+    props.chat();
+  };
 
-//   return (
-//     <div>
-//       <h1>Game Messages</h1>
-//       Enter message here: <input ref={inputRef} type="text" />
-//       <button onClick={onEnterMessage}>Send Message</button>
-//       <ul>
-//         {messages.map((item, index) => <ListItem key={index} name={item} />)}
-//       </ul>
-//       <div>
-//         <h3>All Users (History)</h3>
-//         {userList.map((user, index) => <ListItem key={index} name={user} />)}
-//       </div>
-//     </div>
-//   );
+  const onEnterMessage = () => {
+    if (chatInput != null) {
+      const message = chatInput.current.value;
+      setMessages((prevMessages) => [...prevMessages, message]);
+      socket.emit('message_logged', { message });
+    }
+  };
+
+  useEffect(() => {
+    socket.on('message_logged', (data) => {
+      console.log('message logged');
+      console.log(data);
+      setMessages((prevMessages) => [...prevMessages, data.message]);
+    });
+  });
+
+  return (
+    <div>
+      <button type="button" className="chat" onClick={onChat}>
+        {' '}
+        <img src="https://www.freeiconspng.com/uploads/live-chat-icon-20.png" width="70" height="50" alt="submit" />
+      </button>
+      <div>
+        <h6>Game Messages</h6>
+        Enter message here:
+        {' '}
+        <input ref={chatInput} type="text" />
+        <button type="button" onClick={onEnterMessage}>Send Message</button>
+        <ul>
+          {chatMessages.map((item) => (
+            <li>
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
+
+Chat.propTypes = {
+  chat: PropTypes.objectOf.isRequired,
+};
+
+export default Chat;
--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Results } from './Results';
+import Chat from './Chat';
 
 // These two lines load environmental variables from .env
 const dotenv = require('dotenv');
@@ -9,6 +10,7 @@ dotenv.config();
 
 export function Quiz(props) {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showChat, setChat] = useState(false);
   const {
     game,
     userData,
@@ -25,52 +27,68 @@ export function Quiz(props) {
     setCurrentQuestion(currentQuestion + 1);
   };
 
+  const handleChat = () => {
+    setChat(!showChat);
+  };
+
   return (
     <div>
-      {currentQuestion < 10 ? (
-        <div className="display">
-          <div className="main">
-            <div className="question_number">
-              <span>
-                Question
-                {' '}
-                {' '}
-                {currentQuestion + 1}
-              </span>
-              /
-              {10}
-            </div>
-            <div className="question_text">
-              {game.questions[currentQuestion].question.replace(/&amp;/g, '&')
-                .replace(/&lt;/g, '<')
-                .replace(/&gt;/g, '>')
-                .replace(/&quot;/g, '"')
-                .replace(/&#039;/g, "'")
-                .replace(/&ldquo;/g, '"')
-                .replace(/&rdquo;/g, '"')}
-            </div>
-            <div className="answer_choices">
-              {game.questions[currentQuestion].choices.map((answerChoice) => (
-                <button type="button" className="button" onClick={() => handleAnswerChoiceClick(answerChoice)}>
-                  {answerChoice.replace(/&amp;/g, '&')
-                    .replace(/&lt;/g, '<')
-                    .replace(/&gt;/g, '>')
-                    .replace(/&quot;/g, '"')
-                    .replace(/&#039;/g, "'")
-                    .replace(/&ldquo;/g, '"')
-                    .replace(/&rdquo;/g, '"')}
-                </button>
-              ))}
+      <div>
+        {currentQuestion < 10 ? (
+          <div className="display">
+            <div className="main">
+              <div className="question_number">
+                <span>
+                  Question
+                  {' '}
+                  {' '}
+                  {currentQuestion + 1}
+                </span>
+                /
+                {10}
+              </div>
+              <div className="question_text">
+                {game.questions[currentQuestion].question.replace(/&amp;/g, '&')
+                  .replace(/&lt;/g, '<')
+                  .replace(/&gt;/g, '>')
+                  .replace(/&quot;/g, '"')
+                  .replace(/&#039;/g, "'")
+                  .replace(/&ldquo;/g, '"')
+                  .replace(/&rdquo;/g, '"')}
+              </div>
+              <div className="answer_choices">
+                {game.questions[currentQuestion].choices.map((answerChoice) => (
+                  <button type="button" className="button" onClick={() => handleAnswerChoiceClick(answerChoice)}>
+                    {answerChoice.replace(/&amp;/g, '&')
+                      .replace(/&lt;/g, '<')
+                      .replace(/&gt;/g, '>')
+                      .replace(/&quot;/g, '"')
+                      .replace(/&#039;/g, "'")
+                      .replace(/&ldquo;/g, '"')
+                      .replace(/&rdquo;/g, '"')}
+                  </button>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        <Results
-          answerStats={answerStats}
-          userData={userData}
-          isLogged={isLogged}
-        />
-      )}
+        ) : (
+          <Results
+            answerStats={answerStats}
+            userData={userData}
+            isLogged={isLogged}
+          />
+        )}
+      </div>
+      <div className="chat">
+        { showChat ? (
+          <Chat chat={handleChat} />
+        ) : (
+          <button type="button" className="settings" onClick={handleChat}>
+            {' '}
+            <img src="https://www.freeiconspng.com/uploads/live-chat-icon-20.png" width="70" height="50" alt="submit" />
+          </button>
+        )}
+      </div>
     </div>
   );
 }
