40c88990998481a51c140ba4122f5843e4aaa887
Venkata Sunkara <jnanatejaswini@gmail.com>
2021-04-29 08:31:49 +0000

    implemented multiplayer

---
+++ src/Chat.js
@@ -0,0 +1,33 @@
+// import React, { useState, useEffect, useRef } from 'react';
+// // import PropTypes from 'prop-types';
+// import { socket } from './Socket';
+
+// // These two lines load environmental variables from .env
+// const dotenv = require('dotenv');
+
+dotenv.config();
+
+function Chat() {
+//   const [chatMessages, setMessages] = useState([]);
+//   const chatInput = useRef(null);
+//   const [players, setPlayers] = useState([]);
+
+//   const onEnterMessage = () => {
+//     if (inputRef)
+//   }
+
+//   return (
+//     <div>
+//       <h1>Game Messages</h1>
+//       Enter message here: <input ref={inputRef} type="text" />
+//       <button onClick={onEnterMessage}>Send Message</button>
+//       <ul>
+//         {messages.map((item, index) => <ListItem key={index} name={item} />)}
+//       </ul>
+//       <div>
+//         <h3>All Users (History)</h3>
+//         {userList.map((user, index) => <ListItem key={index} name={user} />)}
+//       </div>
+//     </div>
+//   );
+}
