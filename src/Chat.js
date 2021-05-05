051af4d175886bb0d038086157f997b31af5b8bf
Venkata Sunkara <jnanatejaswini@gmail.com>
2021-05-05 00:02:36 +0000

    added timestamp to chat messages; fixed styling to center messages and player names

--- src/Chat.js
+++ src/Chat.js
@@ -21,30 +21,42 @@ function Chat(props) {
     if (message != null) {
       console.log(chatMessages);
       console.log(players);
+      console.log(userName);
       const chat = chatMessages;
       const newMessage = message;
-      chat.push(`${userName} : ${newMessage}`);
 
+      const today = new Date();
+
+      let hours = today.getHours() > 12 ? today.getHours() - 12 : today.getHours();
+      const amPM = today.getHours() >= 12 ? 'PM' : 'AM';
+      hours = hours < 10 ? `0${hours}` : hours;
+      const minutes = today.getMinutes() < 10 ? `0${today.getMinutes()}` : today.getMinutes();
+      const seconds = today.getSeconds() < 10 ? `0${today.getSeconds()}` : today.getSeconds();
+      const time = `${hours}:${minutes}:${seconds} ${amPM}`;
+
+      chat.push(`${userName} : ${newMessage}`, `${time}`);
       socket.emit('message_logged', { chat });
     }
   }
 
   return (
     <div>
-      <section style={{ display: 'flex', flexDirection: 'row' }}>
+      <section style={{ width: '15%', margin: 'auto' }}>
         <div className="messages">
           <h6>Game Messages</h6>
           <ul id="messages"><Messages data={chatMessages} /></ul>
         </div>
       </section>
-      <div className="playerlist">
-        <h6>Players:</h6>
-        <div>
-          {players.map((player) => (
-            <li>{ player }</li>
-          ))}
+      <section style={{ width: '10%', margin: 'auto' }}>
+        <div className="playerlist">
+          <h6>Players:</h6>
+          <div>
+            {players.map((player) => (
+              <li>{ player }</li>
+            ))}
+          </div>
         </div>
-      </div>
+      </section>
       Enter message here:
       {' '}
       <input ref={chatInput} type="text" />
